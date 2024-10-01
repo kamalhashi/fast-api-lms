465a912d0ff151ecab0d5e17e0e345990af678f1
Kamal Hashi <kamalweheliye@gmail.com>
2024-10-01 10:28:38 +0400

    fixes

--- db/models/user.py
+++ db/models/user.py
@@ -8,7 +8,7 @@ from ..db_setup import Base
 from .mixins import Timestamp
 
 
-class Role(enum.Enum):
+class Role(enum.IntEnum):
     teacher = 1
     student = 2
 
@@ -23,6 +23,9 @@ class User(Timestamp, Base):
 
 
     profile = relationship("Profile", back_populates="owner", uselist=False)
+    student_courses = relationship("StudentCourse", back_populates="student")
+    student_content_blocks = relationship(
+        "CompletedContentBlock", back_populates="student")
 
 
 class Profile(Timestamp, Base):
